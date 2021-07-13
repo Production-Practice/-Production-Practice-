9ffebd189aa37d12f5798faed355c6149c87d76a
xuanhengwd <2456450920@qq.com>
2021-07-13 11:14:22 +0800

    四个图

--- PP/js/app.js
+++ PP/js/app.js
@@ -1,18 +1,17 @@
-function getG(spectrumX,spectrumY,textname,xname,yname)
-{
+function getG4(spectrumX, spectrumY, textname, xname, yname) {
 	var option = {
 		backgroundColor: '#303749',
 		title: {
 			top: '5',
 			left: 'center',
 			text: textname,
-			textStyle:{
+			textStyle: {
 				//文字颜色
-				color:"#ffb052",
-	
+				color: "#ffb052",
+
 			}
 		},
-	
+
 		grid: [{
 			left: '7%',
 			right: '7%',
@@ -22,29 +21,29 @@ function getG(spectrumX,spectrumY,textname,xname,yname)
 		tooltip: {
 			trigger: 'axis'
 		},
-	
+
 		legend: {
-	
+
 			// y: 'bottom',
 			bottom: '10%',
 			x: 'center',
 			// 图例样式修改
 			itemWidth: 12,
-			itemHeight: 8,            // 图例图形高度
+			itemHeight: 8, // 图例图形高度
 			textStyle: {
-				fontSize:12,
+				fontSize: 12,
 				color: 'white'
 			}
 		},
 		xAxis: {
 			name: xname,
 			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
-			data:spectrumX,
+			data: spectrumX,
 			splitLine: {
-	
+
 				show: true,
 				lineStyle: {
-					type: 'solid',  //'dotted'虚线 'solid'实线，
+					type: 'solid', //'dotted'虚线 'solid'实线，
 					color: 'rgba(255,255,255,0.3)'
 				},
 			},
@@ -53,24 +52,24 @@ function getG(spectrumX,spectrumY,textname,xname,yname)
 				lineStyle: {
 					type: 'solid',
 					color: 'rgba(255,255,255,0.3)',
-					width:'1.5'//坐标线的宽度
+					width: '1.5' //坐标线的宽度
 				}
 			},
 			axisLabel: {
 				textStyle: {
-					color: '#fff',//坐标值得具体的颜色
-	
+					color: '#fff', //坐标值得具体的颜色
+
 				}
 			}
-	
+
 		},
 		yAxis: {
 			name: yname,
-		   // boundaryGap: [0.2, 0.2],
+			// boundaryGap: [0.2, 0.2],
 			splitLine: {
 				show: true,
 				lineStyle: {
-					type: 'dotted',  //'dotted'虚线 'solid'实线，
+					type: 'dotted', //'dotted'虚线 'solid'实线，
 					color: 'rgba(255,255,255,0.3)'
 				},
 			},
@@ -80,20 +79,20 @@ function getG(spectrumX,spectrumY,textname,xname,yname)
 				lineStyle: {
 					type: 'solid',
 					color: 'rgba(255,255,255,0.3)',
-					width:'1.5'//坐标线的宽度
+					width: '1.5' //坐标线的宽度
 				}
 			},
 			axisLabel: {
 				textStyle: {
-					color: '#fff',//坐标值得具体的颜色
-	
+					color: '#fff', //坐标值得具体的颜色
+
 				}
 			}
 		},
 		series: [{
 			name: '',
 			/* data: [150, 230, 224, 218, 135, 147, 260], */
-			data:spectrumY,
+			data: spectrumY,
 			type: 'line',
 			color: '#00FF00',
 		}]
@@ -101,21 +100,20 @@ function getG(spectrumX,spectrumY,textname,xname,yname)
 	myChart4.setOption(option);
 }
 
-function getG1(spectrumX,spectrumY,textname,xname,yname)
-{
+function getG3(spectrumX, spectrumY, textname, xname, yname) {
 	var option = {
 		backgroundColor: '#303749',
 		title: {
 			top: '5',
 			left: 'center',
 			text: textname,
-			textStyle:{
+			textStyle: {
 				//文字颜色
-				color:"#ffb052",
-	
+				color: "#ffb052",
+
 			}
 		},
-	
+
 		grid: [{
 			left: '7%',
 			right: '7%',
@@ -125,29 +123,29 @@ function getG1(spectrumX,spectrumY,textname,xname,yname)
 		tooltip: {
 			trigger: 'axis'
 		},
-	
+
 		legend: {
-	
+
 			// y: 'bottom',
 			bottom: '10%',
 			x: 'center',
 			// 图例样式修改
 			itemWidth: 12,
-			itemHeight: 8,            // 图例图形高度
+			itemHeight: 8, // 图例图形高度
 			textStyle: {
-				fontSize:12,
+				fontSize: 12,
 				color: 'white'
 			}
 		},
 		xAxis: {
 			name: xname,
 			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
-			data:spectrumX,
+			data: spectrumX,
 			splitLine: {
-	
+
 				show: true,
 				lineStyle: {
-					type: 'solid',  //'dotted'虚线 'solid'实线，
+					type: 'solid', //'dotted'虚线 'solid'实线，
 					color: 'rgba(255,255,255,0.3)'
 				},
 			},
@@ -156,24 +154,24 @@ function getG1(spectrumX,spectrumY,textname,xname,yname)
 				lineStyle: {
 					type: 'solid',
 					color: 'rgba(255,255,255,0.3)',
-					width:'1.5'//坐标线的宽度
+					width: '1.5' //坐标线的宽度
 				}
 			},
 			axisLabel: {
 				textStyle: {
-					color: '#fff',//坐标值得具体的颜色
-	
+					color: '#fff', //坐标值得具体的颜色
+
 				}
 			}
-	
+
 		},
 		yAxis: {
 			name: yname,
-		   // boundaryGap: [0.2, 0.2],
+			// boundaryGap: [0.2, 0.2],
 			splitLine: {
 				show: true,
 				lineStyle: {
-					type: 'dotted',  //'dotted'虚线 'solid'实线，
+					type: 'dotted', //'dotted'虚线 'solid'实线，
 					color: 'rgba(255,255,255,0.3)'
 				},
 			},
@@ -183,103 +181,379 @@ function getG1(spectrumX,spectrumY,textname,xname,yname)
 				lineStyle: {
 					type: 'solid',
 					color: 'rgba(255,255,255,0.3)',
-					width:'1.5'//坐标线的宽度
+					width: '1.5' //坐标线的宽度
 				}
 			},
 			axisLabel: {
 				textStyle: {
-					color: '#fff',//坐标值得具体的颜色
-	
+					color: '#fff', //坐标值得具体的颜色
+
 				}
 			}
 		},
 		series: [{
 			name: '',
 			/* data: [150, 230, 224, 218, 135, 147, 260], */
-			data:spectrumY,
+			data: spectrumY,
 			type: 'line',
 			color: '#00FF00',
 		}]
 	}
 	myChart3.setOption(option);
 }
-axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-var app=new Vue({
 
-	el:"#app",
+function getG2(spectrumX, spectrumY, textname, xname, yname) {
+	var option = {
+		backgroundColor: '#303749',
+		title: {
+			top: '5',
+			left: 'center',
+			text: textname,
+			textStyle: {
+				//文字颜色
+				color: "#ffb052",
 
-	data:{
-		nodeId:"",
-		equipmentUuid:"",
-		pointId:"",
-		spectrumX:[],
-		spectrumY:[]
+			}
+		},
+
+		grid: [{
+			left: '7%',
+			right: '7%',
+			top: '21%',
+			bottom: '27%'
+		}],
+		tooltip: {
+			trigger: 'axis'
+		},
+
+		legend: {
+
+			// y: 'bottom',
+			bottom: '10%',
+			x: 'center',
+			// 图例样式修改
+			itemWidth: 12,
+			itemHeight: 8, // 图例图形高度
+			textStyle: {
+				fontSize: 12,
+				color: 'white'
+			}
+		},
+		xAxis: {
+			name: xname,
+			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
+			data: spectrumX,
+			splitLine: {
+
+				show: true,
+				lineStyle: {
+					type: 'solid', //'dotted'虚线 'solid'实线，
+					color: 'rgba(255,255,255,0.3)'
+				},
+			},
+			type: 'category',
+			axisLine: {
+				lineStyle: {
+					type: 'solid',
+					color: 'rgba(255,255,255,0.3)',
+					width: '1.5' //坐标线的宽度
+				}
+			},
+			axisLabel: {
+				textStyle: {
+					color: '#fff', //坐标值得具体的颜色
+
+				}
+			}
+
+		},
+		yAxis: {
+			name: yname,
+			// boundaryGap: [0.2, 0.2],
+			splitLine: {
+				show: true,
+				lineStyle: {
+					type: 'dotted', //'dotted'虚线 'solid'实线，
+					color: 'rgba(255,255,255,0.3)'
+				},
+			},
+			type: 'value',
+			//splitNumber: 3,
+			axisLine: {
+				lineStyle: {
+					type: 'solid',
+					color: 'rgba(255,255,255,0.3)',
+					width: '1.5' //坐标线的宽度
+				}
+			},
+			axisLabel: {
+				textStyle: {
+					color: '#fff', //坐标值得具体的颜色
+
+				}
+			}
+		},
+		series: [{
+			name: '',
+			/* data: [150, 230, 224, 218, 135, 147, 260], */
+			data: spectrumY,
+			type: 'line',
+			color: '#00FF00',
+		}]
+	}
+	myChart2.setOption(option);
+}
+
+
+function getG1(spectrumX, spectrumY, textname, xname, yname) {
+	var option = {
+		backgroundColor: '#303749',
+		title: {
+			top: '5',
+			left: 'center',
+			text: textname,
+			textStyle: {
+				//文字颜色
+				color: "#ffb052",
+
+			}
+		},
+
+		grid: [{
+			left: '7%',
+			right: '7%',
+			top: '21%',
+			bottom: '27%'
+		}],
+		tooltip: {
+			trigger: 'axis'
+		},
+
+		legend: {
+
+			// y: 'bottom',
+			bottom: '10%',
+			x: 'center',
+			// 图例样式修改
+			itemWidth: 12,
+			itemHeight: 8, // 图例图形高度
+			textStyle: {
+				fontSize: 12,
+				color: 'white'
+			}
+		},
+		xAxis: {
+			name: xname,
+			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
+			data: spectrumX,
+			splitLine: {
+
+				show: true,
+				lineStyle: {
+					type: 'solid', //'dotted'虚线 'solid'实线，
+					color: 'rgba(255,255,255,0.3)'
+				},
+			},
+			type: 'category',
+			axisLine: {
+				lineStyle: {
+					type: 'solid',
+					color: 'rgba(255,255,255,0.3)',
+					width: '1.5' //坐标线的宽度
+				}
+			},
+			axisLabel: {
+				textStyle: {
+					color: '#fff', //坐标值得具体的颜色
+
+				}
+			}
+
+		},
+		yAxis: {
+			name: yname,
+			// boundaryGap: [0.2, 0.2],
+			splitLine: {
+				show: true,
+				lineStyle: {
+					type: 'dotted', //'dotted'虚线 'solid'实线，
+					color: 'rgba(255,255,255,0.3)'
+				},
+			},
+			type: 'value',
+			//splitNumber: 3,
+			axisLine: {
+				lineStyle: {
+					type: 'solid',
+					color: 'rgba(255,255,255,0.3)',
+					width: '1.5' //坐标线的宽度
+				}
+			},
+			axisLabel: {
+				textStyle: {
+					color: '#fff', //坐标值得具体的颜色
+
+				}
+			}
+		},
+		series: [{
+			name: '',
+			/* data: [150, 230, 224, 218, 135, 147, 260], */
+			data: spectrumY,
+			type: 'line',
+			color: '#00FF00',
+		}]
+	}
+	myChart1.setOption(option);
+}
+
+var app = new Vue({
+
+	el: "#app",
+
+	data: {
+		nodeId: "",
+		equipmentUuid: "",
+		pointId: "",
+		spectrumX: [],
+		spectrumY: [],
+		timer: "",
+		time: [],
+		y1: [],
+		y2: [],
+		isfirst: false
 	},
-	methods:{
-		GetnodeId:function(){
-			that=this;
+	mounted: function() {
+
+		this.GetnodeId();
+		this.timers = setInterval(this.GetnodeId, 5000);
+
+
+
+
+
+	},
+	methods: {
+		GetnodeId: function() {
+			that = this;
 			axios.get("http://39.106.127.16:6793/node/info").then(
-			function(response){
-				console.log(response);
-				console.log(response.data.data[0].nodeId);
-				that.nodeId=response.data.data[0].nodeId;
+				function(response) {
+					/* console.log(response); */
+					console.log("获得nodeid");
+					console.log(response.data.data[0].nodeId);
+					that.nodeId = response.data.data[0].nodeId;
 
-			})
+					if (that.isfirst) {
+						that.GetGraph2();
+						that.GetGraph3();
+						that.GetGraph4();
+					} else {
+						that.GetequipId();
+						that.isfirst = true;
+					}
+
+
+
+				})
 
 		},
 
-		GetequipId:function(){
-			that=this;
-			axios.get("http://39.106.127.16:6793/equipment/node/"+this.nodeId+"/info").then(
-			function(response){
-				console.log(response);
-				that.equipmentUuid=response.data.data[0].equipmentUuid;
-			})
+		GetequipId: function() {
+			that = this;
+			axios.get("http://39.106.127.16:6793/equipment/node/" + this.nodeId + "/info").then(
+				function(response) {
+					console.log("获得equipid");
+					/* console.log(response); */
+					that.equipmentUuid = response.data.data[0].equipmentUuid;
+					that.GetpointId();
+
+				})
 
 		},
 
-		GetpointId:function(){
-			that=this;
-			axios.get("http://39.106.127.16:6793/point/"+this.equipmentUuid+"/detail").then(
-			function(response){
-				console.log(response);
-				that.pointId=response.data.data[0].pointId;
-			})
+		GetpointId: function() {
+			that = this;
+			axios.get("http://39.106.127.16:6793/point/" + this.equipmentUuid + "/detail").then(
+				function(response) {
+					/* 	console.log(response); */
+					that.pointId = response.data.data[0].pointId;
+					console.log("获得pointid");
+					that.GetGraph2();
+					that.GetGraph3();
+					that.GetGraph4();
+
+				})
 		},
 
 
-		GetGraph3:function(){
-			that=this;
-			axios.get("http://39.106.127.16:6793/trend/"+this.equipmentUuid+"/"+this.pointId+"/real_time").then(
-			function(response){
-				console.log(response);
-				console.log(response.data.data.waveValue.waveX)
-				that.spectrumX=response.data.data.waveValue.waveX;
-				that.spectrumY=response.data.data.waveValue.waveY;
-				getG1(that.spectrumX,that.spectrumY,"波形图","[秒]","[um]");
-			}
+
+
+		GetGraph2: function() {
+			that = this;
+			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId +
+				"/real_time").then(
+				function(response) {
+					console.log(response);
+					/* 				console.log(response.data.data.waveValue.waveX) */
+					var dtime = new Date();
+					var year = dtime.getFullYear();
+					var month = dtime.getMonth() + 1;
+					var day = dtime.getDate();
+					var hour = dtime.getHours();
+					var minu = dtime.getMinutes();
+					var sec = dtime.getSeconds();
+					var times = year + "-" + month + "-" + day + " " + hour + ":" + minu + ":" + sec;
+					that.time.push(times);
+					isy2 = response.data.data.rev;
+					that.y2.push(isy2);
+					isy1 = response.data.data.trendValue[0].all;
+					that.y1.push(isy1);
+					getG1(that.time, that.y1, "实时趋势图", "[时间]", "[mm/s2]");
+					getG2(that.time, that.y2, "转速趋势图", "[时间]", "[r/min]");
+					console.log("获得图");
+				}
 			)
-		
-		
 		},
-		
-		
-		GetGraph4:function(){
-			that=this;
-			axios.get("http://39.106.127.16:6793/trend/"+this.equipmentUuid+"/"+this.pointId+"/real_time").then(
-			function(response){
-/* 				console.log(response);
-				console.log(response.data.data.spectrumValue.spectrumX) */
-				that.spectrumX=response.data.data.spectrumValue.spectrumX;
-				that.spectrumY=response.data.data.spectrumValue.spectrumY;
-				getG(that.spectrumX,that.spectrumY,"频谱图","[Hz]","[um]");
-				
-			}
+
+
+		GetGraph3: function() {
+			that = this;
+			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId +
+				"/real_time").then(
+				function(response) {
+					console.log(response);
+					/* 				console.log(response.data.data.waveValue.waveX) */
+					that.spectrumX = response.data.data.waveValue.waveX;
+					that.spectrumY = response.data.data.waveValue.waveY;
+					getG3(that.spectrumX, that.spectrumY, "波形图", "[秒]", "[um]");
+					var dtime = new Date();
+					console.log(dtime);
+					console.log("获得图");
+				}
 			)
 
 
 		},
-		
+
+
+		GetGraph4: function() {
+			that = this;
+			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId +
+				"/real_time").then(
+				function(response) {
+					/* 				console.log(response);
+									console.log(response.data.data.spectrumValue.spectrumX) */
+					that.spectrumX = response.data.data.spectrumValue.spectrumX;
+					that.spectrumY = response.data.data.spectrumValue.spectrumY;
+					getG4(that.spectrumX, that.spectrumY, "频谱图", "[Hz]", "[um]");
+
+				}
+			)
+
+
+		},
+
+
 
 	}
 
