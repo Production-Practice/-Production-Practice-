f11adcc1d29e15fe9bdb5348677c3042ca6eccf9
xuanhengwd <2456450920@qq.com>
2021-07-16 11:31:56 +0800

    前端基础部分基本完成

--- PP/js/app.js
+++ PP/js/app.js
@@ -1,10 +1,10 @@
-function getG4(spectrumX, spectrumY, textname, xname, yname) {
+function getG4(spectrumX, spectrumY) {
 	var option = {
 		backgroundColor: '#303749',
 		title: {
 			top: '5',
 			left: 'center',
-			text: textname,
+			text: "频谱图",
 			textStyle: {
 				//文字颜色
 				color: "#ffb052",
@@ -36,7 +36,7 @@ function getG4(spectrumX, spectrumY, textname, xname, yname) {
 			}
 		},
 		xAxis: {
-			name: xname,
+			name: "[Hz]",
 			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
 			data: spectrumX,
 			splitLine: {
@@ -64,7 +64,7 @@ function getG4(spectrumX, spectrumY, textname, xname, yname) {
 
 		},
 		yAxis: {
-			name: yname,
+			name: "[um]",
 			// boundaryGap: [0.2, 0.2],
 			splitLine: {
 				show: true,
@@ -100,13 +100,13 @@ function getG4(spectrumX, spectrumY, textname, xname, yname) {
 	myChart4.setOption(option);
 }
 
-function getG3(spectrumX, spectrumY, textname, xname, yname) {
+function getG3(spectrumX, spectrumY) {
 	var option = {
 		backgroundColor: '#303749',
 		title: {
 			top: '5',
 			left: 'center',
-			text: textname,
+			text: "波形图",
 			textStyle: {
 				//文字颜色
 				color: "#ffb052",
@@ -138,7 +138,7 @@ function getG3(spectrumX, spectrumY, textname, xname, yname) {
 			}
 		},
 		xAxis: {
-			name: xname,
+			name: "[秒]",
 			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
 			data: spectrumX,
 			splitLine: {
@@ -166,7 +166,7 @@ function getG3(spectrumX, spectrumY, textname, xname, yname) {
 
 		},
 		yAxis: {
-			name: yname,
+			name: "[um]",
 			// boundaryGap: [0.2, 0.2],
 			splitLine: {
 				show: true,
@@ -202,13 +202,13 @@ function getG3(spectrumX, spectrumY, textname, xname, yname) {
 	myChart3.setOption(option);
 }
 
-function getG2(spectrumX, spectrumY, textname, xname, yname) {
+function getG2(spectrumX, spectrumY) {
 	var option = {
 		backgroundColor: '#303749',
 		title: {
 			top: '5',
 			left: 'center',
-			text: textname,
+			text: "转速趋势图",
 			textStyle: {
 				//文字颜色
 				color: "#ffb052",
@@ -240,7 +240,7 @@ function getG2(spectrumX, spectrumY, textname, xname, yname) {
 			}
 		},
 		xAxis: {
-			name: xname,
+			name: "[时间]",
 			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
 			data: spectrumX,
 			splitLine: {
@@ -268,7 +268,7 @@ function getG2(spectrumX, spectrumY, textname, xname, yname) {
 
 		},
 		yAxis: {
-			name: yname,
+			name: "[r/min]",
 			// boundaryGap: [0.2, 0.2],
 			splitLine: {
 				show: true,
@@ -305,13 +305,13 @@ function getG2(spectrumX, spectrumY, textname, xname, yname) {
 }
 
 
-function getG1(spectrumX, Y1, Y2, textname, xname, yname) {
+function getG1(spectrumX, seri) {
 	var option = {
 		backgroundColor: '#303749',
 		title: {
 			top: '5',
 			left: 'center',
-			text: textname,
+			text: "实时趋势图",
 			textStyle: {
 				//文字颜色
 				color: "#ffb052",
@@ -343,7 +343,7 @@ function getG1(spectrumX, Y1, Y2, textname, xname, yname) {
 			}
 		},
 		xAxis: {
-			name: xname,
+			name: "[时间]",
 			/* data: ['2021-01-02 16:00:02', '2021-02-02 16:00:02', '2021-03-02 16:00:02', '2021-04-02 16:00:02', '2021-05-02 16:00:02', '2021-06-02 16:00:02', '2021-07-02 16:00:02'], */
 			data: spectrumX,
 			splitLine: {
@@ -371,7 +371,7 @@ function getG1(spectrumX, Y1, Y2, textname, xname, yname) {
 
 		},
 		yAxis: {
-			name: yname,
+			name: "[mm/s2]",
 			// boundaryGap: [0.2, 0.2],
 			splitLine: {
 				show: true,
@@ -396,32 +396,47 @@ function getG1(spectrumX, Y1, Y2, textname, xname, yname) {
 				}
 			}
 		},
-		series: [{
-			name: "X",
-			/* data: [150, 230, 224, 218, 135, 147, 260], */
-			data: Y1,
-			type: 'line',
-			color: '#00FF00'
+
+		series: seri
 
 
-		},
-
-		{
-			name: "Y",
-			/* data: [150, 230, 224, 218, 135, 147, 260], */
-			data: Y2,
-			type: 'line',
-			color: '#ff1a1a'
+		//  [{
+		// 		name: "X",
+		// 		/* data: [150, 230, 224, 218, 135, 147, 260], */
+		// 		data: Y1,
+		// 		type: 'line',
+		// 		color: '#00FF00'
 
 
-		},
+		// 	},
 
-		]
+		// 	{
+		// 		name: "Y",
+		// 		/* data: [150, 230, 224, 218, 135, 147, 260], */
+		// 		data: Y2,
+		// 		type: 'line',
+		// 		color: '#ff1a1a'
+
+
+		// 	},
+
+		// ]
 	}
 	myChart1.setOption(option);
 }
 
 
+function Graphclear() {
+	myChart1.clear();
+	myChart2.clear();
+}
+
+function clearall() {
+	myChart1.clear();
+	myChart2.clear();
+	myChart3.clear();
+	myChart4.clear();
+}
 
 /* var cbx=document.getElementsByName("p");
 console.log(cbx[0].value);
@@ -435,11 +450,13 @@ var app = new Vue({
 
 	data: {
 
+		series: [],
 		xclc: true,
 		yclc: false,
 		nodeId: "",
 		equipmentUuid: "",
-		pointId: "",
+		pointId: [],
+
 		spectrumX: [],
 		spectrumY: [],
 		waveX: [],
@@ -457,46 +474,66 @@ var app = new Vue({
 		show4: false,
 		show5: false,
 		imgurl1: "../images/rightsan",
-		imgurl2: "../images/downsan"
+		imgurl2: "../images/downsan",
+		flag: 0,
+		isstop: false
 
 	},
-	mounted: function () {
+	mounted: function() {
 
 		this.GetnodeId();
-		this.timers = setInterval(this.GetnodeId, 5000);
+		this.timer = setInterval(this.GetnodeId, 3000);
 
 	},
 	methods: {
 
-		Get: function () {
-			// this.GetnodeId();
-			if (this.xclc || this.yclc) {
+		// Get: function() {
+		// 	// this.GetnodeId();
+		// 	if (this.xclc || this.yclc) {
 
-				this.GetnodeId();
+		// 		this.GetnodeId();
 
-			}
+		// 	}
 
 
-		},
+		// },
 
-		xclick: function () {
+		xclick: function() {
 
 			this.xclc = !this.xclc;
+			Graphclear();
 			this.time = [];
 			this.y1 = [];
 			this.y12 = [];
-			getG1(this.time, this.y1, this.y12, "实时趋势图", "[时间]", "[mm/s2]")
+			this.y2 = [];
+			this.series = [];
+			// getG1(this.time, this.series);
+			// getG2(this.time, this.y2);
+			if (this.xclc == false && this.yclc == false) {
+				clearall();
+			}
+			this.GetnodeId();
+
 		},
 
-		yclick: function () {
+		yclick: function() {
 
 			this.yclc = !this.yclc;
+			Graphclear();
 			this.time = [];
 			this.y1 = [];
 			this.y12 = [];
-			getG1(this.time, this.y1, this.y12, "实时趋势图", "[时间]", "[mm/s2]")
+			this.y2 = [];
+			this.series = [];
+			if (this.xclc == false && this.yclc == false) {
+				clearall();
+			}
+			this.GetnodeId();
+			// getG1(this.time, this.series);
+			// getG2(this.time, this.y2);
 		},
-		click1: function () {
+
+		click1: function() {
 			if (this.show2) {
 				this.show2 = !this.show2;
 			}
@@ -508,7 +545,7 @@ var app = new Vue({
 				this.show5 = !this.show5;
 			this.show1 = !this.show1;
 		},
-		click2: function () {
+		click2: function() {
 			if (this.show3)
 				this.show3 = !this.show3;
 			if (this.show4)
@@ -517,32 +554,51 @@ var app = new Vue({
 				this.show5 = !this.show5;
 			this.show2 = !this.show2;
 		},
-		click3: function () {
+		click3: function() {
 			if (this.show4)
 				this.show4 = !this.show4;
 			if (this.show5)
 				this.show5 = !this.show5;
 			this.show3 = !this.show3;
 		},
-		click4: function () {
+		click4: function() {
 			if (this.show5)
 				this.show5 = !this.show5;
 			this.show4 = !this.show4;
 		},
-		click5: function () {
+		click5: function() {
 			this.show5 = true;
 		},
 
 
-		GetnodeId: function () {
+		stop: function() {
+			this.isstop = !this.isstop;
+			console.log(this.isstop);
+			if (this.isstop) {
+				clearInterval(this.timer);
+			} else {
+
+				Graphclear();
+				Graphclear();
+				this.time = [];
+				this.y1 = [];
+				this.y12 = [];
+				this.y2 = [];
+				this.series = [];
+				this.GetnodeId();
+				this.timer = setInterval(this.GetnodeId, 3000);
+			}
+		},
+		GetnodeId: function() {
 			that = this;
 			axios.get("http://39.106.127.16:6793/node/info").then(
-				function (response) {
+				function(response) {
 					/* console.log(response); */
 					console.log("获得nodeid");
-					console.log(response.data.data[0].nodeId);
+					// console.log(response.data.data[0].nodeId);
 					that.nodeId = response.data.data[0].nodeId;
-					if (that.xclc || that.yclc) {
+
+					if (that.xclc == true || that.yclc == true) {
 						if (that.isfirst) {
 							that.GetGraph2();
 							that.GetGraph3();
@@ -551,24 +607,28 @@ var app = new Vue({
 							that.GetequipId();
 							that.isfirst = true;
 						}
-
-
+					} else {
+						that.waveX = [];
+						that.waveY = [];
+						that.spectrumX = [];
+						that.spectrumY = [];
+						clearall();
+						getG1(this.time, this.series);
+						getG2(this.time, this.y2);
+						getG3(that.waveX, that.waveY);
+						getG4(that.spectrumX, that.spectrumY);
 					}
-
-
-
-
 
 				})
 
 		},
 
-		GetequipId: function () {
+		GetequipId: function() {
 			that = this;
 			axios.get("http://39.106.127.16:6793/equipment/node/" + this.nodeId + "/info").then(
-				function (response) {
+				function(response) {
 					console.log("获得equipid");
-					/* console.log(response); */
+					console.log(response);
 					that.equipmentUuid = response.data.data[0].equipmentUuid;
 					that.GetpointId();
 
@@ -576,12 +636,13 @@ var app = new Vue({
 
 		},
 
-		GetpointId: function () {
+		GetpointId: function() {
 			that = this;
 			axios.get("http://39.106.127.16:6793/point/" + this.equipmentUuid + "/detail").then(
-				function (response) {
+				function(response) {
 					console.log(response);
-					that.pointId = response.data.data[0].pointId;
+					that.pointId.push(response.data.data[0].pointId);
+					that.pointId.push(response.data.data[1].pointId);
 					console.log("获得pointid");
 					that.GetGraph2();
 					that.GetGraph3();
@@ -593,72 +654,130 @@ var app = new Vue({
 
 
 
-		GetGraph2: function () {
+		GetGraph2: function() {
 			that = this;
-			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId +
+			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId[0] +
 				"/real_time").then(
-					function (response) {
-						console.log(response);
-						/* 				console.log(response.data.data.waveValue.waveX) */
-						var dtime = new Date();
-						var year = dtime.getFullYear();
-						var month = dtime.getMonth() + 1;
-						var day = dtime.getDate();
-						var hour = dtime.getHours();
-						var minu = dtime.getMinutes();
-						var sec = dtime.getSeconds();
-						var times = year + "-" + month + "-" + day + " " + hour + ":" + minu + ":" + sec;
-						that.time.push(times);
+				function(response) {
 
-						isy2 = response.data.data.rev;
-						that.y2.push(isy2);
-						isy1 = response.data.data.trendValue[0].all;
+					axios.get("http://39.106.127.16:6793/trend/" + that.equipmentUuid + "/" + that
+						.pointId[1] +
+						"/real_time").then(
+						function(response){
+							
+						}
+						)
 
 
+					console.log(response);
+					/* 				console.log(response.data.data.waveValue.waveX) */
 
-						that.y1.push(isy1);
-						that.y12.push(isy2);
-						getG1(that.time, that.y1, that.y12, "实时趋势图", "[时间]", "[mm/s2]");
-						getG2(that.time, that.y2, "转速趋势图", "[时间]", "[r/min]");
-						console.log("获得图");
+
+					var dtime = new Date();
+					var year = dtime.getFullYear();
+					var month = dtime.getMonth() + 1;
+					var day = dtime.getDate();
+					var hour = dtime.getHours();
+					var minu = dtime.getMinutes();
+					var sec = dtime.getSeconds();
+					var times = year + "-" + month + "-" + day + " " + hour + ":" + minu + ":" + sec;
+					that.time.push(times);
+					isy2 = response.data.data.rev;
+					that.y2.push(isy2);
+
+
+
+					isy1 = response.data.data.trendValue[0].all;
+					that.y1.push(isy1);
+					that.y12.push(isy2);
+
+
+
+					var yx1 = {
+						name: "X",
+						data: that.y1,
+						type: 'line',
+						color: '#00FF00'
+
+					};
+					var yy1 = {
+						name: "Y",
+						data: that.y12,
+						type: 'line',
+						color: '#ff1a1a'
+					};
+
+
+					that.series[0] = yx1;
+					that.series[1] = yy1;
+
+					if (that.xclc == true && that.yclc == false) {
+						that.series.splice(1, 1);
+					} else if (that.xclc == false && that.yclc == true) {
+						that.series.splice(0, 1);
+					} else if (that.xclc == false && that.yclc == false) {
+						that.series.splice(1, 1);
+						that.series.splice(0, 1);
 					}
-				)
+
+					// 				if(that.xclc==true&&that.yclc==true)
+					// 				{
+					// /* 					that.series[0]=yx1;
+					// 					that.series[1]=yy1; */
+					// 				}
+					// 				else if(that.xclc==true&&that.yclc==false)
+					// 				{
+
+					// 				}
+
+					// that.series[0]=yx1;
+					// that.series[1]=yy1;
+					/* 					that.series.push(yx1);
+										that.series.push(yy1); */
+					console.log(that.series);
+					getG1(that.time, that.series);
+					getG2(that.time, that.y2);
+					console.log("获得图12");
+				}
+			)
 		},
 
 
-		GetGraph3: function () {
+
+		GetGraph3: function() {
 			that = this;
 			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId +
 				"/real_time").then(
-					function (response) {
-						console.log(response);
-						/* 				console.log(response.data.data.waveValue.waveX) */
-						that.waveX = response.data.data.waveValue.waveX;
-						that.waveY = response.data.data.waveValue.waveY;
-						getG3(that.waveX, that.waveY, "波形图", "[秒]", "[um]");
-						/* 					var dtime = new Date();
-											console.log(dtime); */
-						console.log("获得图");
-					}
-				)
+				function(response) {
+					console.log(response);
+					/* 				console.log(response.data.data.waveValue.waveX) */
+					that.waveX = response.data.data.waveValue.waveX;
+					that.waveY = response.data.data.waveValue.waveY;
+					getG3(that.waveX, that.waveY);
+					/* 					var dtime = new Date();
+										console.log(dtime); */
+					console.log("获得图3");
+				}
+			)
 
 
 		},
 
 
-		GetGraph4: function () {
+		GetGraph4: function() {
 			that = this;
 			axios.get("http://39.106.127.16:6793/trend/" + this.equipmentUuid + "/" + this.pointId +
 				"/real_time").then(
-					function (response) {
-						/* 				console.log(response);
-										console.log(response.data.data.spectrumValue.spectrumX) */
-						that.spectrumX = response.data.data.spectrumValue.spectrumX;
-						that.spectrumY = response.data.data.spectrumValue.spectrumY;
-						getG4(that.spectrumX, that.spectrumY, "频谱图", "[Hz]", "[um]");
+				function(response) {
+					/* 				console.log(response);
+									console.log(response.data.data.spectrumValue.spectrumX) */
+					that.spectrumX = response.data.data.spectrumValue.spectrumX;
+					that.spectrumY = response.data.data.spectrumValue.spectrumY;
+					getG4(that.spectrumX, that.spectrumY);
 
-					}
-				)
+
+				}
+			)
 
 
 		},
